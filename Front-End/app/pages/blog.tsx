4065a584f34849376fbd19af5b75692f968fa558
Varun <>
2024-02-20 18:35:27 +0530

    Integrated Sanity

--- Front-End/app/pages/blog.tsx
+++ Front-End/app/pages/blog.tsx
@@ -1,22 +1,50 @@
 import React from 'react'
 import Blogcard from '../components/blogcard'
 import { createClient } from "next-sanity";
+import imageUrlBuilder from "@sanity/image-url"
+import Image from 'next/image';
 
-function Blog() {
+const client = createClient({
+  projectId: "old3apdr",
+  dataset: "production",
+  apiVersion: "2022-03-25",
+  useCdn: false
+});
+const builder=imageUrlBuilder(client)
+export function urlFor(source:any){
+    return builder.image(source)
+}
+async function getData() {
+  const query = `*[_type=="blog"]{
+    name,
+    description,
+      poster,
+      releaseDate
+  }`;
+  const data = await client.fetch(query);
+  return data;
+} 
+async function Blog() {
+  const data=await getData();
   return (
     <>
             <div className='flex flex-col text-white p-8 gap-6 justify-center'>
               <div className=''>
                   <h1 className='text-5xl font-bold'>Latest</h1>
                   <p>A blog created with Next.js and Tailwind.css</p>
-                 
               </div>
+              
            <hr className='border-b border-gray-800 w-11/12 self-center'/>
               <div>
                   <ul className="grid grid-cols-1 xl:grid-cols-3 gap-y-10 gap-x-6 items-start ">
-                      <Blogcard/>
-                      <Blogcard/>
-                      <Blogcard/>
+                      {data.map((item: any,idx: number)=>(
+                        <div key={idx}>
+                            <h1>{item.name}</h1>
+                            <p>{item.description}</p>
+                            
+                        </div>
+                      ) 
+                      )}
                   </ul>
               </div>
           </div>
@@ -25,19 +53,3 @@ function Blog() {
   )
 }
 export default Blog
-
-const client = createClient({
-  projectId: "lqz08o01",
-  dataset: "production",
-  apiVersion: "2022-03-25",
-  useCdn: false
-});
-export async function getStaticProps() {
-  const pets = await client.fetch(`*[_type == "pet"]`);
-
-  return {
-    props: {
-      pets
-    }
-  };
-}
